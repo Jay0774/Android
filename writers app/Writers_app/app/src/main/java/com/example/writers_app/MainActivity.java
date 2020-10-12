d0ce715c2bc95ef1dd258fdf414fcbd600b7a5f4
JAYESH BUDHWANI <54534558+Jay0774@users.noreply.github.com>
2020-10-12 16:35:08 +0530

    new app

---
+++ writers app/Writers_app/app/src/main/java/com/example/writers_app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.writers_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
